2dc30c299ac95d7c48cdf801de27bbfc2318713e
Mike Norgate <mike@polystream.com>
2019-01-09 14:26:17 +0000

    Allow all azure config options to be overridden in config

--- cloud-provider.sh
+++ cloud-provider.sh
@@ -4,15 +4,20 @@ AZURE_META_URL="http://169.254.169.254/metadata/instance/compute"
 AZURE_CLOUD_CONFIG_PATH="/etc/kubernetes/cloud-config"
 
 set_azure_config() {
-  local az_resources_group=$(curl  -s -H Metadata:true "${AZURE_META_URL}/resourceGroupName?api-version=2017-08-01&format=text")
-  local az_subscription_id=$(curl -s -H Metadata:true "${AZURE_META_URL}/subscriptionId?api-version=2017-08-01&format=text")
-  local az_location=$(curl  -s -H Metadata:true "${AZURE_META_URL}/location?api-version=2017-08-01&format=text")
-  local az_vm_name=$(curl -s -H Metadata:true "${AZURE_META_URL}/name?api-version=2017-08-01&format=text")
+  local az_resources_group=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .resourceGroup)
+  local az_subscription_id=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .subscriptionId)
+  local az_location=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .location)
   local azure_cloud=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .cloud)
   local azure_client_id=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .aadClientId)
   local azure_client_secret=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .aadClientSecret)
   local azure_tenant_id=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .tenantId)
   local az_vm_nsg=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .securityGroupName)
+  local az_vnet_resource_group=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .vnetResourceGroup)
+  local az_subnet_name=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .subnetName)
+  local az_vnet_name=$(cat "$AZURE_CLOUD_CONFIG_PATH" | jq -r .vnetName)
+
+  local az_vm_resources_group=$(curl  -s -H Metadata:true "${AZURE_META_URL}/resourceGroupName?api-version=2017-08-01&format=text")
+  local az_vm_name=$(curl -s -H Metadata:true "${AZURE_META_URL}/name?api-version=2017-08-01&format=text")
 
   # setting correct login cloud
   if [ "${azure_cloud}" = "null" ] || [ "${azure_cloud}" = "" ]; then
@@ -23,13 +28,26 @@ set_azure_config() {
   # login to Azure
   az login --service-principal -u ${azure_client_id} -p ${azure_client_secret} --tenant ${azure_tenant_id} 2>&1 > /dev/null
 
-  local az_vm_nic=$(az vm nic list -g ${az_resources_group} --vm-name ${az_vm_name} | jq -r .[0].id | cut -d "/" -f 9)
-  local az_subnet_name=$(az vm nic show -g ${az_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic}| jq -r .ipConfigurations[0].subnet.id| cut -d"/" -f 11)
-  local az_vnet_name=$(az vm nic show -g ${az_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic}| jq -r .ipConfigurations[0].subnet.id| cut -d"/" -f 9)
-  local az_vnet_resource_group=$(az vm nic show -g ${az_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic}| jq -r .ipConfigurations[0].subnet.id| cut -d"/" -f 5)
+  if [ -z "$az_resources_group" ] ; then
+    az_resources_group="$az_vm_resources_group"
+  fi
+
+  local az_vm_nic=$(az vm nic list -g ${az_vm_resources_group} --vm-name ${az_vm_name} | jq -r .[0].id | cut -d "/" -f 9)
+
+  if [ -z "$az_subnet_name" ] ; then
+    az_subnet_name=$(az vm nic show -g ${az_vm_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic}| jq -r .ipConfigurations[0].subnet.id| cut -d"/" -f 11)
+  fi
+
+  if [ -z "$az_vnet_name" ] ; then
+    az_vnet_name=$(az vm nic show -g ${az_vm_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic}| jq -r .ipConfigurations[0].subnet.id| cut -d"/" -f 9)
+  fi
+
+  if [ -z "$az_vnet_resource_group" ] ; then
+    az_vnet_resource_group=$(az vm nic show -g ${az_vm_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic}| jq -r .ipConfigurations[0].subnet.id| cut -d"/" -f 5)
+  fi
 
   if [ -z "$az_vm_nsg" ] ; then
-    az_vm_nsg=$(az vm nic show -g ${az_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic} | jq -r .networkSecurityGroup.id | cut -d "/" -f 9)
+    az_vm_nsg=$(az vm nic show -g ${az_vm_resources_group} --vm-name ${az_vm_name} --nic ${az_vm_nic} | jq -r .networkSecurityGroup.id | cut -d "/" -f 9)
   fi
 
   az logout 2>&1 > /dev/null
