23954167c01fe9ae30a709c8fc8240e00d334459
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2019-01-30 00:39:14 +0200

    Add portmap plugin to weave

--- weave-plugins-cni.sh
+++ weave-plugins-cni.sh
@@ -3,6 +3,8 @@
 # deploy loopback cni
 mkdir -p /opt/cni/bin
 mv /tmp/loopback /opt/cni/bin
+mv /tmp/portmap /opt/cni/bin
+chmod 755 /opt/cni/bin/portmap
 while true; do
-  sleep 10
+  sleep 100
 done
