8100b9d06beb0174a431539bae0beaabb874a574
Frank Mai <frank@rancher.com>
2019-08-28 19:07:38 +0800

    Fixed getting wrong internal address in host-gw
    **Problem:**
    Windows kubelet could not detect the appropriate network adapter by itself.
    
    **Solution:**
    Patch the --node-ip with default network adapter if not pass internal address.
    
    **Issue:**
    https://github.com/rancher/rancher/issues/22475

--- windows/sidecar.ps1
+++ windows/sidecar.ps1
@@ -126,10 +126,10 @@ if ($networkConfigObj)
                 }
                 "host-gw" {
                     $networkAddress = ""
-                    if ($nodeAddress)  {
-                        $networkAddress = $nodeAddress
-                    } elseif ($nodeInternalAddress) {
+                    if ($nodeInternalAddress) {
                         $networkAddress = $nodeInternalAddress
+                    } elseif ($nodeAddress)  {
+                        $networkAddress = $nodeAddress
                     }
 
                     $networkMetadataJSON = wins.exe cli net get --address "$networkAddress"
